ed86cd02edbd5d3cdb92acb88a2bcf56dcd4bd85
DrRobotikk <tangenrobin@gmail.com>
2024-05-08 23:35:51 +0200

    Added the character

---
+++ Dungeon RPG/Scripts/Characters/Player/PlayerDashState.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class PlayerDashState : Node
+{
+    private Player characterNode;
+    [Export] private Timer dashTimerNode;
+    public override void _Ready()
+    {
+        characterNode = GetOwner<Player>();
+        dashTimerNode.Timeout += HandleDashTimeout;
+        
+    }
+
+    public override void _Notification(int what)
+    {
+        base._Notification(what);
+
+        if (what == 5001)
+        {
+            characterNode.animPlayerNode.Play(GameGonstants.ANIM_DASH);
+            dashTimerNode.Start();
+            
+        }
+        
+    }
+    private void HandleDashTimeout(){
+        characterNode.stateMachineNode.SwitchState<PlayerIdleState>();
+    }
+}
